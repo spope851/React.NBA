7477d109e12a5266307cf7ecfebade0f6648c53a
Spencer Pope <34104632+spope851@users.noreply.github.com>
2020-08-30 19:06:47 -0400

    Update player.js

--- Start/src/components/player.js
+++ Start/src/components/player.js
@@ -9,16 +9,14 @@ class Player extends React.Component{
     }
   }
   render() {
-    const toggleDetails = () => {
-      this.setState({details: !this.state.details})
-    }
+
     return (
       <div  className="card col-1">
         <div className="card-header">
-          <span onClick={(e)=>toggleDetails(e)}>{this.props.name}</span>
+
         </div>
         <div className="card-body">
-          {this.state.details && <PlayerDetails data={this.props.details}/>}
+
         </div>
       </div>
     );
